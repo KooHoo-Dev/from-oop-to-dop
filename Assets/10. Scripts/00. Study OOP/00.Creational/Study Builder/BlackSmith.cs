ee2803b6bef42aa02806604eb329a008d67d482e
99jinmo <99jinmo@gmail.com>
2026-03-31 10:41:51 +0900

    빌더패턴 #1

---
+++ Assets/10. Scripts/00. Study OOP/00.Creational/Study Builder/BlackSmith.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Study.OOP.Builder
+{
+    public class BlackSmith
+    {
+        public Sword GenerateSword(Grade grade)
+        {
+            return new Sword();
+        }
+    }
+}
